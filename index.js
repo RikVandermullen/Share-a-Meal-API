e026f57755346aedadb426618b3d5653151ab5fa
RikVandermullen <rik.vandermullen@gmail.com>
2022-04-26 10:06:38 +0200

    Routes added

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const userRouter = require('./src/routes/user.routes');
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
-let database = [];
-let id = 0;
 
 app.all("*", (req, res, next) => {
   const method = req.method;
@@ -14,115 +12,7 @@ app.all("*", (req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    result: "Hello World",
-  });
-});
-
-//Adds new User to database if emailaddress is unqiue
-app.post("/api/user", (req, res) => {
-  let user = req.body;
-  id++;
-  user = {
-    id,
-    ...user,
-  };
-  let email = database.filter((item) => item.emailAdress == user.emailAdress);
-  if (email.length == 0) {
-    database.push(user);
-    res.status(201).json({
-      status: 201,
-      result: user,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with email ${user.emailAdress} already exists`,
-    });
-  }
-});
-
-//Retrieves User profile (not implemented)
-app.get("/api/user/profile", (req, res) => {
-  res.status(401).json({
-    status: 401,
-    result: "This functionality has not yet been implemented",
-  });
-});
-
-//Retrieves User info based on id paramater
-app.get("/api/user/:userId", (req, res) => {
-  const userId = req.params.userId;
-  console.log(`User met ID ${userId} gezocht`);
-  let user = database.filter((item) => item.id == userId);
-  if (user.length > 0) {
-    console.log(user);
-    res.status(200).json({
-      status: 200,
-      result: user,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${userId} not found`,
-    });
-  }
-});
-
-//Updates User info based on id paramater
-app.put("/api/user/:userId", (req, res) => {
-  const id = req.params.userId;
-  const newUserInfo = req.body;
-  let userArray = database.filter((item) => item.id == id);
-  if (userArray.length > 0) {
-    let foundUser = userArray[0];
-    let index = database.indexOf(foundUser);
-    user = {
-        id,
-        ...newUserInfo,
-      };
-    database[index] = user;
-    res.status(201).json({
-      status: 201,
-      result: user,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${id} was not found and not updated`,
-    });
-  }
-});
-
-//Deletes User based on id paramater
-app.delete("/api/user/:userId", (req, res) => {
-  const id = req.params.userId;
-  let userArray = database.filter((item) => item.id == id);
-  if (userArray.length > 0) {
-    let foundUser = userArray[0];
-    let index = database.indexOf(foundUser);
-    database.splice(index, 1);
-    res.status(200).json({
-      status: 200,
-      result: `User with ID ${id} is deleted`,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `User with ID ${id} was not found and not deleted`,
-    });
-  }
-});
-
-//Retrieves all Users info
-app.get("/api/user", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    result: database,
-  });
-});
+app.use(userRouter)
 
 //Return error for incorrect routes
 app.all("*", (req, res) => {
